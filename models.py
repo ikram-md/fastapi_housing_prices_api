4cd28fbe1f8e76574d7f26d37f7dd15da568a5de
ikram-md <driver2011@protonmail.com>
2022-09-26 14:34:51 +0100

    added the user model

--- models.py
+++ models.py
@@ -1,9 +1,14 @@
 from db_config import Base
-from sqlalchemy import Column, Integer, String, Boolean, Float, PrimaryKeyConstraint, TIMESTAMP, text
+from sqlalchemy import Column, Integer, String, Boolean, Float, TIMESTAMP, text
 
 
 # we store all of our table models in this file.
 class House(Base):
+    """_summary_
+
+    Args:
+        Base (_type_): _description_
+    """
     __tablename__ = "houses"
 
     id = Column(Integer, primary_key=True,nullable=False)
@@ -13,4 +18,17 @@ class House(Base):
     size = Column(Integer, nullable=False)
     desc= Column(String, server_default='No description was provided.')
     #timestamp column
+    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
+
+
+class User(Base):
+    """_summary_
+
+    Args:
+        Base (_type_): _description_
+    """
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, nullable=False)
+    email = Column(String, unique=True, nullable=False)
+    password = Column(String, nullable=False)
     created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
