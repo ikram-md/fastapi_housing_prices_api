41eef99273dcca74db44363018ba9d58ecb0a04a
ikram-md-smartinia <jazzthedreameryeah@gmail.com>
2023-02-08 20:37:58 +0100

    chore(root): init postgres connection in main app

--- main.py
+++ main.py
@@ -1,27 +1,11 @@
-import time
-from fastapi import FastAPI, HTTPException, status, Depends
+from fastapi import FastAPI
 import models
-import psycopg2
-from db_config import engine, get_db
-# this will help us retrieve the column name for the tables
-from psycopg2.extras import RealDictCursor
+from db_config import engine
+
 from routers import houses, users, auth
 
 app = FastAPI()
 
-# Connect to Postgres database.
-
-while True:
-    try:
-        conn = psycopg2.connect(host="localhost", database="postgres", user="postgres", password="postgres159753",
-                                cursor_factory=RealDictCursor)
-        cursor = conn.cursor()
-        print("Database connected successfully.")
-        break
-
-    except Exception as error:
-        print("Connection to the database failed", error)
-        time.sleep(2)
 
 # ensures that our models are in sync.
 models.Base.metadata.create_all(bind=engine)
